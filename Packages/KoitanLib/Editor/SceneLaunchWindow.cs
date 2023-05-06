d552c753016890ce20df007b5fbac922c0d81e58
DESKTOP-IB4U5C3\tk_ka <tk_kakutasu@yahoo.co.jp>
2023-05-06 23:33:54 +0900

    SceneLuncher追加

---
+++ Packages/KoitanLib/Editor/SceneLaunchWindow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace KoitanLib
+{
+    namespace Editor
+    {
+        public class SceneLaunchWindow : EditorWindow
+        {
+            private SceneAsset[] sceneArray;
+            private Vector2 scrollPos = Vector2.zero;
+
+            [MenuItem("KoitanLib/Scene Launcher")]
+            static void Open()
+            {
+                GetWindow<SceneLaunchWindow>("SceneLauncher");
+            }
+
+            void OnFocus()
+            {
+                
+            }
+
+            void OnGUI()
+            {
+                EditorGUI.BeginDisabledGroup(EditorApplication.isPlaying);
+                this.scrollPos = EditorGUILayout.BeginScrollView(this.scrollPos);
+                EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
+                foreach (var scene in scenes)
+                {
+                    string[] strs = scene.path.Split('/');
+
+                    string sceneName = strs[strs.Length - 1].Replace(".unity", string.Empty);
+                    if (GUILayout.Button(sceneName))
+                    {
+                        if(EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+                        {
+                            EditorSceneManager.OpenScene(scene.path);
+                        }
+                    }
+                }
+                EditorGUILayout.EndScrollView();
+                EditorGUI.EndDisabledGroup();
+            }
+        }
+    }
+}
