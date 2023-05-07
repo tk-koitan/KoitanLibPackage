fd629d351f9ae81db9bd839f1adbc51fd01f66f4
DESKTOP-IB4U5C3\tk_ka <tk_kakutasu@yahoo.co.jp>
2023-05-07 19:24:46 +0900

    コマンド更新

--- Packages/KoitanLib/Editor/DebugSceneAdder.cs
+++ Packages/KoitanLib/Editor/DebugSceneAdder.cs
@@ -2,15 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System.Linq;
 
 public static class DebugSceneAdder
 {
     [MenuItem("KoitanLib/AddScene")]
     private static void AddScene()
     {
-        Debug.Log("SceneAdd");
         var scenes = EditorBuildSettings.scenes;
-        ArrayUtility.Add(ref scenes, new EditorBuildSettingsScene("Packages/com.koitan.koitanlib/KoitanLib/Scenes/DebugScene.unity", true));
+        string debugScenePath = "Packages/com.koitan.koitanlib/KoitanLib/Scenes/DebugScene.unity";
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            if (scenes[i].path == debugScenePath)
+            {
+                Debug.Log("既にデバッグシーンがあります");
+                return;
+            }
+        }
+        ArrayUtility.Add(ref scenes, new EditorBuildSettingsScene(debugScenePath, true));
         EditorBuildSettings.scenes = scenes;
+        Debug.Log("デバッグシーンを追加しました");
+    }
+
+    [MenuItem("KoitanLib/RemoveScene")]
+    private static void RemoveScene()
+    {
+        var scenes = EditorBuildSettings.scenes;
+        string debugScenePath = "Packages/com.koitan.koitanlib/KoitanLib/Scenes/DebugScene.unity";
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            if (scenes[i].path == debugScenePath)
+            {
+                ArrayUtility.RemoveAt(ref scenes, i);
+                EditorBuildSettings.scenes = scenes;
+                Debug.Log("デバッグシーンを消しました");
+                return;
+            }
+        }
+        Debug.Log("デバッグシーンがありません");
     }
 }
