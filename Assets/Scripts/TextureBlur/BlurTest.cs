ded78d3c1f0cb5efddf230bcbc0859a8e4996569
DESKTOP-IB4U5C3\tk_ka <tk_kakutasu@yahoo.co.jp>
2023-07-09 23:58:43 +0900

    Fade仮実装

---
+++ Assets/Scripts/TextureBlur/BlurTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class BlurTest : MonoBehaviour
+{
+    [SerializeField]
+    float radius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var spr = GetComponent<SpriteRenderer>();
+        var tex = BuildTexture.CreateBlurTexture(spr.sprite.texture, radius);
+        spr.sprite = Sprite.Create(tex, spr.sprite.rect, new Vector2(0.5f, 0.5f), 100.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
